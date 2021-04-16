6ef02084bd6a58b84bcbcad5cbf933cc7933b438
spartan289 <60476934+spartan289@users.noreply.github.com>
2021-04-16 16:26:43 +0530

    first

---
+++ travello/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Destination
+# Create your views here.
+def index(request):
+    dest1 = Destination()
+    dest1.name = 'Mumbai'
+    dest1.desc = 'The class'
+    dest1.price = '506'
+    return render(request, "index.html",{'dest1': dest1})
